9b3bf81bbc5ea84d94236b09ad15089f9abb50a8
Chen Chen <chenchen.bme@gmail.com>
2015-11-11 10:50:30 -0600

    Updated IBAMR Install Bash
    Fixed problem associated with SAMRAI installation

--- GB-DragStudy.sh
+++ GB-DragStudy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #MSUB -A p20751
-#MSUB -l nodes=6:ppn=8
+#MSUB -l nodes=6:ppn=4
 #MSUB -l walltime=04:00:00
 #MSUB -N GB_Drag_Study
 #MSUB -o outlog
@@ -15,4 +15,4 @@ cd $PBS_O_WORKDIR
 ##restart_number = 
 ##mpiexec ./main3d input3d.Knifefish restart_IB3d $restart_number -stokes_ksp_monitor_true_residual -stokes_ksp_converged_reason -stokes_ksp_rtol 1.0e-5 > KSP.log
 
-mpiexec ./main3d input3d.Knifefish -stokes_ksp_monitor_true_residual -stokes_ksp_converged_reason -stokes_ksp_rtol 1.0e-5 > KSP.log
+mpiexec ./main3d input3d.Knifefish -stokes_ksp_monitor_true_residual -stokes_ksp_converged_reason -stokes_ksp_rtol 1.0e-8
